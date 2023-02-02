c68487d7d8fdac8f61533f2eb1275c118886c274
mobin-newroz <ahmobin@newroztech.com>
2023-02-02 17:47:59 +0600

    feat : update crud repo, add more functionalities

--- src/Interfaces/CrudInterface.php
+++ src/Interfaces/CrudInterface.php
@@ -5,4 +5,19 @@ namespace Ahmobin\LaravelLpa\Interfaces;
 interface CrudInterface
 {
     public function findAll();
+
+    public function findById(int $id, array $relations = []);
+
+    public function findByKeyValue(string $key, string $value, array $relations = []);
+
+    public function getByKeyValue(string $key, string $value);
+
+    public function store(array $data);
+
+    public function update(array $data, int $id);
+
+    public function deleteById(int $id);
+
+    public function deleteAllByKeyValue(string $key, string $value);
+
 }
--- src/Repositories/CrudRepository.php
+++ src/Repositories/CrudRepository.php
@@ -4,6 +4,7 @@ namespace Ahmobin\LaravelLpa\Repositories;
 
 use Illuminate\Database\Eloquent\Model;
 use Ahmobin\LaravelLpa\Interfaces\CrudInterface;
+use Illuminate\Support\Collection;
 
 class CrudRepository implements CrudInterface
 {
@@ -14,8 +15,47 @@ class CrudRepository implements CrudInterface
         $this->model = $model;
     }
 
-    public function findAll()
+    public function findAll(): collection
     {
         return $this->model::all();
+    }
+
+    public function findById(int $id, array $relations = []): ?Model
+    {
+        return $this->model::with($relations)->find($id);
+    }
+
+    public function findByKeyValue(string $key, string $value, array $relations = []): ?Model
+    {
+        return $this->model::with($relations)->where($key, $value)->first();
+    }
+
+    public function getByKeyValue(string $key, string $value): collection
+    {
+        return $this->model::where($key, $value)->get();
+    }
+
+    public function store(array $data): ?Model
+    {
+        return $this->model::create($data);
+    }
+
+    public function update(array $data, int $id): bool
+    {
+        $item = $this->findById($id);
+        return $item->update($data);
+    }
+
+
+    public function deleteById(int $id): void
+    {
+        $item = $this->findById($id);
+        $item->delete();
+    }
+
+    public function deleteAllByKeyValue(string $key, string $value): void
+    {
+        $item = $this->findByKeyValue($key, $value);
+        $item->delete();
     }
 }
